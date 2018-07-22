87c509bbb855a1a85f09abb58a7daff7808833e8
CT <reddum@gmail.com>
2018-07-23 00:08:43 +0800

    test sync

--- messages/index.js
+++ messages/index.js
@@ -26,7 +26,7 @@ bot.localePath(path.join(__dirname, './locale'));
 bot.set('storage', tableStorage);
 
 bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
+    session.send('You said::: ' + session.message.text);
 });
 
 if (useEmulator) {
