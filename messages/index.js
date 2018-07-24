59ca21888b34eac0d2d8ef17249646524f39891f
CT <reddum@gmail.com>
2018-07-24 20:03:58 +0800

    add image detection

--- messages/index.js
+++ messages/index.js
@@ -2,6 +2,7 @@
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
+var request = require('request');
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
@@ -19,22 +20,71 @@ var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 // bot.set('storage', tableStorage);
 
+const PredictImageUrl = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/14f16d27-19d8-46fa-9516-d1f0d7865813/url?iterationId=9e2ff709-26b4-461d-8d53-f841ff3b289a"
+const PredictImage = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/14f16d27-19d8-46fa-9516-d1f0d7865813/image?iterationId=9e2ff709-26b4-461d-8d53-f841ff3b289a"
+const predictionKey = process.env['PREDICITON_KEY']
+
+// const PredictImage = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/436679fe-5a12-4c17-a882-36b3eaeee242/image?iterationId=8ebb899f-779d-4d8f-8d17-3f72d4d6b22e"
+// const predictionKey = "9462f12ddd784b11be961574cc96de8e"
+
+
+if (predictionKey == null) {
+    console.log("please set custom vision key")
+    console.log("$export PREDICITON_KEY=${key}")
+    return;
+}
+
+var imageDetection = async (session, name, url) => {
+
+    // session.send({
+    //     text: await imageDetection(attachment.contentUrl, null),
+    //     attachments: [
+    //         {
+    //             contentType: attachment.contentType,
+    //             contentUrl: attachment.contentUrl,
+    //             name: attachment.name
+    //         }
+    //     ]
+    // });
+
+    var headers = {
+        'Prediction-Key': predictionKey,
+        'Content-Type': 'application/octet-stream'
+    }
+
+    request.get(url).pipe(
+        request.post({ url: PredictImage, headers: headers }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var info = JSON.parse(body);
+                console.log(info);
+                processDetectionInfo(session, info);
+            }
+            else {
+                console.log(response.statusCode)
+            }
+        })
+    )
+}
+
+var processDetectionInfo = (session, result) => {
+    var foods = []
+    for (var prediction of result.predictions) {
+        var tagName = prediction.tagName
+        var probability = parseFloat(prediction.probability)
+        if (probability > 0.15) {
+            foods.push(tagName)
+        }
+    }
+    session.send("我看到了" + foods.join())
+}
+
 // Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector, function (session) {
     var msg = session.message;
     if (msg.attachments && msg.attachments.length > 0) {
-     // Echo back attachment
-     var attachment = msg.attachments[0];
-        session.send({
-            text: "You sent:",
-            attachments: [
-                {
-                    contentType: attachment.contentType,
-                    contentUrl: attachment.contentUrl,
-                    name: attachment.name
-                }
-            ]
-        });
+        // Echo back attachment
+        var attachment = msg.attachments[0];
+        imageDetection(session, attachment.name, attachment.contentUrl)
     } else {
         // Echo back users text
         session.send("You said: %s", session.message.text);
@@ -44,10 +94,10 @@ var bot = new builder.UniversalBot(connector, function (session) {
 if (useEmulator) {
     var restify = require('restify');
     var server = restify.createServer();
-    server.listen(3978, function() {
+    server.listen(3978, function () {
         console.log('test bot endpont at http://localhost:3978/api/messages');
     });
-    server.post('/api/messages', connector.listen());    
+    server.post('/api/messages', connector.listen());
 } else {
     module.exports = connector.listen();
 }
